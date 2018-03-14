08d0924199219115e1406a5e87256359e041ed21
Stefano Pian <s.pian@zeus-lab.com>
2018-03-14 19:47:54 +0100

    More tests

--- Cvdm.ErrorHandling.Tests/NamespaceOverride.fs
+++ Cvdm.ErrorHandling.Tests/NamespaceOverride.fs
@@ -1,17 +1,24 @@
-/// Testing that the auto-opened name do not collide with user-defined ones
+/// Testing that the auto-opened name do not override any user-defined ones...
 module SomeUserNamespace.SomeUserModule
 
 module AsyncResult = 
-  let map = ""
+  let defaultValue = ""
 
 module Result = 
-  let map = ""
+  let defaultValue = ""
 
 let result = ""
 let asyncResult = ""
 
-let _ : string = AsyncResult.map
-let _ : string = Result.map
+let _ : string = Result.defaultValue
+let _ : string = AsyncResult.defaultValue
 let _ : string = result
 let _ : string = asyncResult
 
+/// ... but that the user may still use the library ones by opening them explicitly
+open Cvdm.ErrorHandling
+
+let _ : 'a -> Async<Result<'a, 'b>> -> Async<'a> = AsyncResult.defaultValue
+let _ : 'a -> Result<'a, 'b> -> 'a = Result.defaultValue
+let _ : ResultBuilder = result
+let _ : AsyncResultBuilder = asyncResult
