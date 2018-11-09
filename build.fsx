9e8f19a8a34e71458cdd494aa2825826e298b5d4
Christer van der Meeren <cmeeren@protonmail.com>
2018-11-09 18:56:04 +0100

    Add FAKE build script

---
+++ build.fsx
@@ -0,0 +1,51 @@
+#r "paket: groupref Build //"
+#load ".fake/build.fsx/intellisense.fsx"
+open Fake.Core
+open Fake.Core.TargetOperators
+open Fake.DotNet
+open Fake.IO
+open Fake.IO.FileSystemOperators
+open Fake.IO.Globbing.Operators
+
+Target.create "Clean" (fun _ ->
+  !! "**/bin"
+  ++ "**/obj"
+  |> Shell.deleteDirs
+)
+
+Target.create "Build" (fun _ ->
+  !! "src/**/*.*proj"
+  |> Seq.iter (DotNet.build (fun x ->
+      { x with
+          Configuration = DotNet.BuildConfiguration.Release
+      }
+  ))
+)
+
+Target.create "Test" (fun _ ->
+  "src/Cvdm.ErrorHandling.Tests"
+  |> DotNet.test (fun x ->
+      { x with
+          Configuration = DotNet.BuildConfiguration.Release
+      }
+  )
+)
+
+Target.create "Pack" (fun _ ->
+  "src/Cvdm.ErrorHandling"
+  |> DotNet.pack (fun x ->
+      { x with
+          Configuration = DotNet.BuildConfiguration.Release
+      }
+  )
+)
+
+Target.create "Default" ignore
+
+"Clean"
+  ==> "Build"
+  ==> "Test"
+  ==> "Pack"
+  ==> "Default"
+
+Target.runOrDefault "Default"
